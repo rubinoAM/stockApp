8def4854064d78c518d91c2fd967a195f2d000ae
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-18 13:27:28 -0500

    Switched to QUOTE

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 $(document).ready(()=>{
     //Do the damn thing!
     const stockForm = document.querySelector('.stock-form');
-    console.log(stockForm);
 
-    const url = `https://api.iextrading.com/1.0/stock/goog/chart`;
+    //Endpoint: A web accessible URL that responds with data
+    const url = `https://api.iextrading.com/1.0/stock/goog/quote`;
 
     //getJSON() takes two args: 1) Where to get JSON 2) Function to Return
     $.getJSON(url,(jsonData)=>{
         console.log(jsonData);
+        $('#stock-body').html(`
+            <tr>
+                <td>${jsonData.symbol}</td>
+                <td>${jsonData.companyName}</td>
+                <td>${jsonData.iexAskPrice}</td>
+                <td>${jsonData.iexBidPrice}</td>
+                <td>${jsonData.change}</td>
+            </tr>
+        `);
     });
 })
