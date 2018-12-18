5c0e95f953abee696e2a0e3d3cae60f2db69079b
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-18 13:40:53 -0500

    All stocks accessible

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 $(document).ready(()=>{
     //Do the damn thing!
     const stockForm = document.querySelector('.stock-form');
+    console.log(stockForm);
 
-    //Endpoint: A web accessible URL that responds with data
-    const url = `https://api.iextrading.com/1.0/stock/goog/quote`;
+    $('.stock-form').submit((event)=>{
+        //Make sure the browser doesn't send the form to another page
+        event.preventDefault();
+        console.log(event);
 
-    //getJSON() takes two args: 1) Where to get JSON 2) Function to Return
-    $.getJSON(url,(jsonData)=>{
-        console.log(jsonData);
-        $('#stock-body').html(`
-            <tr>
-                <td>${jsonData.symbol}</td>
-                <td>${jsonData.companyName}</td>
-                <td>${jsonData.high}</td>
-                <td>${jsonData.low}</td>
-                <td>${jsonData.change}</td>
-            </tr>
-        `);
+        const symbol = $('#symbol').val();
+
+        //Endpoint: A web accessible URL that responds with data
+        const url = `https://api.iextrading.com/1.0/stock/${symbol}/quote`;
+
+        //getJSON() takes two args: 1) Where to get JSON 2) Function to Return
+        $.getJSON(url,(jsonData)=>{
+            console.log(jsonData);
+            $('#stock-body').html(`
+                <tr>
+                    <td>${jsonData.symbol}</td>
+                    <td>${jsonData.companyName}</td>
+                    <td>${jsonData.high}</td>
+                    <td>${jsonData.low}</td>
+                    <td>${jsonData.change}</td>
+                </tr>
+            `);
+        });
     });
 })
